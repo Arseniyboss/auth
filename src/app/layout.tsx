c0302f8d2a6e5db1409977aad5bf20314420f0c2
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-08-02 19:56:44 +0200

    add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css'
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 import StyledComponentsRegistry from '../lib/registry'
 import Header from '@components/header/Header'
 
 type Props = {
   children: ReactNode
+}
+
+export const metadata: Metadata = {
+  title: 'Auth',
 }
 
 const RootLayout = ({ children }: Props) => {
